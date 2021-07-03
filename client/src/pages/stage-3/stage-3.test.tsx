cd5ccf4fe4aa5ccfa5df894885d3cf7dba94c7dd
Denijar <denijar@gmail.com>
2021-07-03 14:43:53 +1200

    adds login page with login component

--- client/src/pages/stage-3/stage-3.test.tsx
+++ client/src/pages/stage-3/stage-3.test.tsx
@@ -2,7 +2,7 @@ import React from "@testing-library/react";
 import { shallow } from "enzyme";
 import Stage3 from ".";
 
-it("renders a Maze component", () => {
+it("renders a Stage 3 text", () => {
   const shallowComponent = shallow(<Stage3 />);
   expect(shallowComponent).toIncludeText("Stage 3");
 });
