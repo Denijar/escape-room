ecfe5fe16c79b503158d432e5ff346d7456c8b59
Denijar <denijar@gmail.com>
2021-07-03 15:15:58 +1200

    adds login form with hardcoded validation. Directs to first sync page

--- client/src/app/app.test.tsx
+++ client/src/app/app.test.tsx
@@ -2,6 +2,11 @@ import React from "@testing-library/react";
 import { shallow } from "enzyme";
 import App from ".";
 
+it("renders the login page", () => {
+  const shallowComponent = shallow(<App />);
+  expect(shallowComponent).toContainExactlyOneMatchingElement("LoginPage");
+});
+
 it("renders the Stage1 page", () => {
   const shallowComponent = shallow(<App />);
   expect(shallowComponent).toContainExactlyOneMatchingElement("Stage1");
--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const history = useHistory();
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  // TODO validate with backend
+  const correctUsername = "1234";
+  const correctPassword = "DogLover";
+
+  const handleSubmit = () => {
+    if (username === correctUsername && password === correctPassword) {
+      history.push("/stage_1");
+    }
+  };
+
+  const handleChangeUsername = (event: FormEvent<HTMLInputElement>) => {
+    setUsername(event.currentTarget.value);
+  };
+
+  const handleChangePassword = (event: FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+      <label htmlFor="username">
+        Username:
+        <input id="username" type="text" value={username} onChange={handleChangeUsername} />{" "}
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input id="password" type="password" value={password} onChange={handleChangePassword} />{" "}
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
 
 export default Login;
