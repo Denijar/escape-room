48f96099bc5e0583e64060384d4aa873f8097384
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-07-06 23:48:08 +1200

    adds DoorKey component with colour and key holder props

--- client/src/components/door-key/door-key.tsx
+++ client/src/components/door-key/door-key.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleRight, faKey } from "@fortawesome/free-solid-svg-icons";
+import styles from "./door-key.module.scss";
 
-function DoorKey() {
-  return <div>Key</div>;
+interface DoorKeyProps {
+  nextStageURL: string;
+  colour: string;
+  keyHolder: string;
+}
+
+function DoorKey({ nextStageURL, colour, keyHolder }: DoorKeyProps) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(nextStageURL);
+  };
+
+  return (
+    <>
+      <div className={styles.background}>
+        <div style={{ color: colour }}>
+          <FontAwesomeIcon className={styles.key} icon={faKey} size="8x" />
+        </div>
+        <div className={styles.text}>
+          <div>{`You have found the ${colour} key!`}</div>
+          <div>{`Ask ${keyHolder} to give it to you`}</div>
+        </div>
+        <div className={styles.next}>
+          <FontAwesomeIcon className={styles.next} icon={faArrowCircleRight} size="3x" onClick={handleClick} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DoorKey;
--- client/src/pages/stage-4/stage-4.tsx
+++ client/src/pages/stage-4/stage-4.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import DoorKey from "../../components/door-key";
 
 function Stage4() {
-  return <DoorKey />;
+  return <DoorKey nextStageURL="/stage_4" colour="yellow" keyHolder="Denise" />;
 }
 
 export default Stage4;
